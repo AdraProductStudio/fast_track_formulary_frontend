aa37029b2fc3d8c4d56c674811367fd90d8f608a
krishna-kumarr <kumarkrishna11231@gmail.com>
2025-12-19 13:41:17 +0530

    FIX: Mob responsive card padding adjusted - 2

--- app/(routes)/(user)/[search_results]/page.js
+++ app/(routes)/(user)/[search_results]/page.js
@@ -64,14 +64,14 @@ export default function searchDetails({ params }) {
                         </div>
                     </div>
 
-                    <div className="card-body med_details_page_content">
-                        <div className="d-flex flex-wrap">
+                    <div className="card-body w-100 px-md-4">
+                        <div className="med_details_page_content d-flex flex-wrap">
                             <div className="col">
                                 <div className="card h-100 custom_card_shadow">
-                                    <div className="card-body">
-                                        <h6>Current medications</h6>
+                                    <div className="card-body w-100 px-2 px-md-4">
+                                        <h6 className="my-3 text-center">Current medications</h6>
 
-                                        <div className="card mt-4 border rounded-4 overflow-hidden">
+                                        <div className="card border rounded-4 overflow-hidden">
                                             <div className="card-header border-0 current_medication_left_side_header">
                                                 Summary of coverage requirements
                                             </div>
@@ -122,7 +122,7 @@ export default function searchDetails({ params }) {
 
                             <div className="alternate_medications_width">
                                 <div className="card h-100 custom_card_shadow">
-                                    <div className="card-body">
+                                    <div className="card-body w-100 px-2 px-md-4">
                                         <div className="custom_card_shadow rounded-3">
                                             <div className="p-3 d-flex flex-wrap align-items-center">
                                                 <div className="col">
@@ -216,76 +216,78 @@ export default function searchDetails({ params }) {
                         </ButtonComponent>
                     </div>
 
-                    <div className="card-body med_details_page_content">
-                        {data?.spinner ?
-                            <div className="d-flex flex-wrap mt-3">
-                                <AlternativeMedicationCardSkeleton className="col-12 col-md-6 col-lg-4 col-xxl-3 p-2" />
-                            </div>
-                            :
-                            data?.data?.primary?.drugCount ?
+                    <div className="card-body w-100">
+                        <div className="med_details_page_content">
+                            {data?.spinner ?
                                 <div className="d-flex flex-wrap mt-3">
-                                    {data?.data?.primary?.results?.map((medication, index) => (
-                                        <div className="col-12 col-md-6 col-xxl-3 p-2" key={index} onClick={() => selected_primary_medication(medication?.drugId || "")}>
-                                            <div className="card custom_card_shadow h-100 med_suggestion_list_card">
-                                                {medication?.tierLevel && (<div className="med_suggestion_list_card_tier">  Tier {medication?.tierLevel || "N/A"} </div>)}
-
-                                                <div className={`card-header border-0 bg-transparent pt-3 ${medication?.tierLevel ? "mt-4" : ""}`}>
-                                                    <p className="mb-2"> {medication?.drugName || ""}</p>
-                                                    <p className="para_three mb-2">{medication?.formularyName || ""}</p>
-                                                    <div className="border-bottom">
-                                                        <p className="para_three">Drug ID: {medication?.drugId || ""}</p>
-                                                    </div>
-
-                                                </div>
-
-                                                <div className="card-body py-1 d-grid align-content-center">
-                                                    <div className="d-flex flex-wrap">
-                                                        <div className="mb-2">
-                                                            <p className="para_two mb-1" style={{ color: medication?.isActive ? "#008236" : "red" }}> {medication?.isActive ? "Available" : "Not Available"}</p>
-                                                        </div>
-
-                                                        <div className="col-12 medical_condition_name mb-3">
-                                                            <Image src={vector_icons.guard_icon} width={18} height={18} alt="time_icon" className="me-2 mb-1" />
-                                                            <div>
-                                                                <p className="para_three mb-0">- {medication?.medicalConditionName1 || ""}</p>
-                                                                <p className="para_three mb-0">- {medication?.medicalConditionName2 || ""}</p>
-                                                            </div>
-                                                        </div>
-
-                                                        <div className="col-6 border-end pe-3">
-                                                            <h6 className="text-primary">
-                                                                <Image src={vector_icons.time_icon} width={18} height={18} alt="time_icon" className="me-2 mb-1" />
-                                                                Limit
-                                                            </h6>
-                                                            <p className="para_three mb-2">{medication?.quantityLimitText || "N/A"}</p>
-                                                        </div>
-
-                                                        <div className="col ps-3 req_content">
-                                                            <h6>
-                                                                <Image src={vector_icons.tag_icon} width={18} height={18} alt="time_icon" className="me-2 mb-1" />
-                                                                Req
-                                                            </h6>
-                                                            <p className="para_three mb-2 word_break_all">{medication?.requirementsText || "N/A"}</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                {medication?.drugTier && (
-                                                    <div className="card-footer border-0 bg-transparent border-top py-3">
-                                                        <div className="custom_card_success">
-                                                            {medication?.drugTier || "N/A"}
-                                                        </div>
-                                                    </div>
-                                                )}
-                                            </div>
-                                        </div>
-                                    ))}
+                                    <AlternativeMedicationCardSkeleton className="col-12 col-md-6 col-lg-4 col-xxl-3 p-2" />
                                 </div>
                                 :
-                                <div className="col-12 h-75 d-flex flex-wrap justify-content-center align-items-center">
-                                    <p className="text-center mt-5">No results found</p>
-                                </div>
-                        }
+                                data?.data?.primary?.drugCount ?
+                                    <div className="d-flex flex-wrap mt-3">
+                                        {data?.data?.primary?.results?.map((medication, index) => (
+                                            <div className="col-12 col-md-6 col-xxl-3 py-2 p-md-2" key={index} onClick={() => selected_primary_medication(medication?.drugId || "")}>
+                                                <div className="card custom_card_shadow h-100 med_suggestion_list_card">
+                                                    {medication?.tierLevel && (<div className="med_suggestion_list_card_tier">  Tier {medication?.tierLevel || "N/A"} </div>)}
+
+                                                    <div className={`card-header border-0 bg-transparent pt-3 ${medication?.tierLevel ? "mt-4" : ""}`}>
+                                                        <p className="mb-2"> {medication?.drugName || ""}</p>
+                                                        <p className="para_three mb-2">{medication?.formularyName || ""}</p>
+                                                        <div className="border-bottom">
+                                                            <p className="para_three">Drug ID: {medication?.drugId || ""}</p>
+                                                        </div>
+
+                                                    </div>
+
+                                                    <div className="card-body py-1 d-grid align-content-center">
+                                                        <div className="d-flex flex-wrap">
+                                                            <div className="mb-2">
+                                                                <p className="para_two mb-1" style={{ color: medication?.isActive ? "#008236" : "red" }}> {medication?.isActive ? "Available" : "Not Available"}</p>
+                                                            </div>
+
+                                                            <div className="col-12 medical_condition_name mb-3">
+                                                                <Image src={vector_icons.guard_icon} width={18} height={18} alt="time_icon" className="me-2 mb-1" />
+                                                                <div>
+                                                                    <p className="para_three mb-0">- {medication?.medicalConditionName1 || ""}</p>
+                                                                    <p className="para_three mb-0">- {medication?.medicalConditionName2 || ""}</p>
+                                                                </div>
+                                                            </div>
+
+                                                            <div className="col-6 border-end pe-3">
+                                                                <h6 className="text-primary">
+                                                                    <Image src={vector_icons.time_icon} width={18} height={18} alt="time_icon" className="me-2 mb-1" />
+                                                                    Limit
+                                                                </h6>
+                                                                <p className="para_three mb-2">{medication?.quantityLimitText || "N/A"}</p>
+                                                            </div>
+
+                                                            <div className="col ps-3 req_content">
+                                                                <h6>
+                                                                    <Image src={vector_icons.tag_icon} width={18} height={18} alt="time_icon" className="me-2 mb-1" />
+                                                                    Req
+                                                                </h6>
+                                                                <p className="para_three mb-2 word_break_all">{medication?.requirementsText || "N/A"}</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    {medication?.drugTier && (
+                                                        <div className="card-footer border-0 bg-transparent border-top py-3">
+                                                            <div className="custom_card_success">
+                                                                {medication?.drugTier || "N/A"}
+                                                            </div>
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    :
+                                    <div className="col-12 h-75 d-flex flex-wrap justify-content-center align-items-center">
+                                        <p className="text-center mt-5">No results found</p>
+                                    </div>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
